c6580ccde7b0c41bb53a26bd98574be4987c123e
nuluh <dam.ar@outlook.com>
2022-12-26 23:09:06 +0700

    First commit

--- prob_calculator.py
+++ prob_calculator.py
@@ -1,8 +1,34 @@
-import copy
 import random
 # Consider using the modules imported above.
 
+
 class Hat:
+    def __init__(self, **kwargs):
+        self.red = kwargs.get('red', 0)
+        self.green = kwargs.get('green', 0)
+        self.blue = kwargs.get('blue', 0)
+        self.yellow = kwargs.get('yellow', 0)
+        self.orange = kwargs.get('orange', 0)
+        self.black = kwargs.get('black', 0)
+        self.pink = kwargs.get('pink', 0)
+        self.striped = kwargs.get('striped', 0)
+
+        self.contents = []
+        for colors in kwargs:
+            for counts in range(kwargs[colors]):
+                self.contents.append(colors)
+
+    def draw(self, numbers):
+        drawn_balls = []
+        for _ in range(numbers):
+            drawn_balls.append(
+                self.contents[random.randint(0, len(self.contents)-1)])
+        # remove drawn balls in self.contents
+        for i in drawn_balls:
+            self.contents.remove(i)
+        return drawn_balls
 
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    # for i in range(num_experiments):
+    hat.draw(num_balls_drawn)
