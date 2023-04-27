7cf69592d33493d66313048537ec30c584fbdc30
nuluh <dam.ar@outlook.com>
2023-04-27 21:45:11 +0700

    uncomment main.py

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
 # This entrypoint file to be used in development. Start by reading README.md
+# import prob_calculator
 import prob_calculator
 from unittest import main
 
 prob_calculator.random.seed(95)
-hat = prob_calculator.Hat(blue=4, red=2, green=6)
-# print(hat.contents)
-# print(hat.draw(2))
-# print(hat.contents)
-probability = prob_calculator.experiment(
-    hat=hat,
-    expected_balls={"blue": 2,
-                    "red": 1},
-    num_balls_drawn=4,
-    num_experiments=3000)
-print("Probability:", probability)
+hat = prob_calculator.Hat(blue=3, red=2, green=6)
 
-# Run unit tests automatically
-# main(module='test_module', exit=False)
+probability = prob_calculator.experiment(hat=hat,
+                                         expected_balls={"blue": 2,
+                                                         "green": 1},
+                                         num_balls_drawn=4,
+                                         num_experiments=1000)
+print("Probability:", probability)
+main(module='test_module', exit=False)
